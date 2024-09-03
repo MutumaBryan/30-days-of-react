24dd570d757de1550edff35f56a505f63966ab1e
MutumaBryan <brayantim22@gmail.com>
2024-09-03 22:26:33 +0300

    chore: Exercise for day 3, create-react-app setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const headerStyles = {
+  textAlign: "center",
+};
+
+const header = (
+  <header style={headerStyles}>
+    <h1>Subscribe</h1>
+  </header>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const mainStyles = {
+  padding: '5px',
+  textAlign: 'center',
+};
+
+const main = (
+  <main style={mainStyles}>
+    <p>Sign up with your email address to receive news and updates</p>
+    <form style={{padding: '10px', textAlign: 'center'}}>
+      <input id="first-name" type="text" placeholder="First name" style={{padding: '15px', borderRadius: '5px', border: '0', margin: '5px', width: '20%'}}></input>
+      <input id="last-name" type="text" placeholder="Last name"style={{padding: '15px', borderRadius: '5px', border: '0', margin: '5px', width: '20%'}}></input>
+      <input id="email" type="email" placeholder="Email" style={{padding: '15px',borderRadius: '5px', border: '0', margin: '5px', width: '20%'}}></input>
+      <br></br>
+      <input type="submit" value="Subscribe" style={{fontFamily: `Montserrat`, backgroundColor: 'red', width: '20%', textAlign: 'center', padding: '2vh 3vw', borderRadius: '7px', border: '0', margin: '10px'}}></input>
+    </form>
+  </main>
+);
+
+const appStyles = {
+  backgroundColor: "#61dafb",
+  fontFamily: `Montserrat`,
+  padding: '10% 10%',
+  margin: '5vh 5vh',
+  borderRadius: '7px'
+};
+
+// JSX element, app
+const app = (
+  <div style={appStyles}>
+    {header}
+    {main}
+  </div>
+);
+
+root.render(app);
